7da6b9f6420a0ab272b33203f75206d07b513353
Marc Akbar <akbar13@live.com>
2018-11-16 19:18:48 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,6 +14,40 @@ class TicTacToe
   [2,4,6]  # TR -> BL diagonal
   ]
 
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-"*11
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-"*11
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
 
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
 
+  def move(index, current_player)
+    @board[index] = current_player
+  end
+
+  def position_taken?(location)
+    @board[location] != " " && @board[location] != ""
+  end
+
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index, current_player(board))
+      display_board
+    else
+      turn
+    end
+  end
+  
 end
